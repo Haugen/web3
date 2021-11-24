bb71b6e8f32ee26060808e893a845c5f7a4e58bf
thaugen <thaugen@google.com>
2021-11-24 10:01:32 -0500

    First test compiling a contract with solc

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ A repository for notes on Solidity, as well as small practice projects.
 - https://remix.ethereum.org/ Online and chain integrated Solidity text editor.
 - https://openzeppelin.com/ Library for secure and community vetted contracts.
 - https://infura.io/ Provides Ethereum nodes to query during development.
+- https://web3js.readthedocs.io/ Web3 JS documentation.
+- https://docs.soliditylang.org/ Solidity documentation.
--- inbox/compile.js
+++ inbox/compile.js
@@ -1,1 +1,30 @@
 const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+const input = {
+  language: 'Solidity',
+  sources: {
+    Inbox: {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      '*': {
+        '*': ['*'],
+      },
+    },
+  },
+};
+
+const output = solc.compile(JSON.stringify(input));
+const contractOutput = JSON.parse(output).contracts.Inbox;
+fs.writeFileSync('./output.json', output);
+
+console.log(contractOutput);
+
+module.exports = contractOutput;
