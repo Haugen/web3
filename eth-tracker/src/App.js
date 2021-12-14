c5e6d748e9b4d4c2cf60a919b33a0e1603f0b1fb
Haugen <hej@tobiashaugen.se>
2021-12-13 20:17:56 -0500

    Playing with ethers.js to build a tx tracker.

---
+++ eth-tracker/src/App.js
@@ -0,0 +1,85 @@
+import { useEffect, useState } from 'react';
+
+import { ethers } from 'ethers';
+
+import './App.css';
+
+function App() {
+  let [txs, setTxs] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const apiKey = 'YN422X22MT73712RCWGMX8ZQ11W7G2MEY2';
+      const etherscan = new ethers.providers.EtherscanProvider(
+        'mainnet',
+        apiKey
+      );
+
+      const wallet = '0x79308680fA5297401f335C39e7F440C35C6f6Fc5';
+      const history = await etherscan.getHistory(wallet);
+      const lastThree = history.slice(Math.max(history.length - 3, 0));
+      lastThree.reverse();
+
+      const txsData = lastThree.map((tx) => {
+        return {
+          txHash: tx.hash,
+          block: tx.blockNumber,
+          timestamp: tx.timestamp,
+          from: tx.from,
+          to: tx.to,
+        };
+      });
+
+      setTxs(txsData);
+    })();
+  }, []);
+
+  const formatAddress = (address) => {
+    const first = address.slice(0, 5);
+    const last = address.slice(Math.max(address.length - 4, 0));
+    return `${first}...${last}`;
+  };
+
+  const formatTx = (hash) => {
+    return hash.slice(0, 15) + '...';
+  };
+
+  const formatTime = (timestamp) => {};
+
+  const formatTxs = (txs) => {
+    const formatted = [];
+    for (const tx of txs) {
+      formatted.push(
+        <tr>
+          <td>{formatTx(tx.txHash)}</td>
+          <td>{tx.block}</td>
+          <td>{tx.timestamp}</td>
+          <td>{formatAddress(tx.from)}</td>
+          <td>{formatAddress(tx.to)}</td>
+        </tr>
+      );
+    }
+    return formatted;
+  };
+
+  return (
+    <div className="App">
+      <h1>ETH tracker</h1>
+      <p>Why use Etherscan when you have ETH tracker?</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Txn Hash</th>
+            <th>Block</th>
+            <th>Age</th>
+            <th>From</th>
+            <th>To</th>
+          </tr>
+        </thead>
+        <tbody>{formatTxs(txs)}</tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
